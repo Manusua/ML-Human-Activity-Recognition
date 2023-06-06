de80e2f8a190c5e0ee8251ca0fef8014b050e732
MANUEL.SUAREZ.ROMAN.930827 <manuel.suarez@csic.es>
2023-06-06 09:41:31 +0200

    rading dataframe

--- training.py
+++ training.py
@@ -1,3 +1,18 @@
 import tensorflow
-
+import pandas as pd
+import os
 path_data = "data/sensors-activity-recognition-dataset-shoaib/DataSet"
+
+#Count number of files in directory
+_, _, files = next(os.walk("/usr/lib"))
+num_files = len(files)
+
+#Create a common dataframe
+df = pd.DataFrame()
+for i in range(num_files):
+    print(path_data + files[i])
+    df_tmp = pd.read_csv(path_data + files[i], header=1)
+    df = pd.concat([df, df_tmp])
+
+# View top 5 rows of dataframe
+df.head()
