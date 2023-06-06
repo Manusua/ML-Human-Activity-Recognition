971a52835f9f0215ebb9a020ce15c80c48e545b2
MANUEL.SUAREZ.ROMAN.930827 <manuel.suarez@csic.es>
2023-06-06 17:29:33 +0200

    Notebook creation and header and plots

--- training.py
+++ training.py
@@ -1,18 +1,25 @@
-import tensorflow
+#import tensorflow
 import pandas as pd
 import os
-path_data = "data/sensors-activity-recognition-dataset-shoaib/DataSet"
-
-#Count number of files in directory
-_, _, files = next(os.walk("/usr/lib"))
-num_files = len(files)
+import matplotlib as plt
+import numpy as np
+path_data = "./data/sensors-activity-recognition-dataset-shoaib/DataSet/"
 
 #Create a common dataframe
 df = pd.DataFrame()
-for i in range(num_files):
-    print(path_data + files[i])
-    df_tmp = pd.read_csv(path_data + files[i], header=1)
-    df = pd.concat([df, df_tmp])
+#Count number of files in directory
+for _, _, files in os.walk(path_data):
+    for file in files:
+        if file.endswith('.csv'):
+            print("Gathering data from " + path_data + file)
+            df_tmp = pd.read_csv(path_data + file, header=1)
+            df = pd.concat([df, df_tmp])
 
-# View top 5 rows of dataframe
-df.head()
+for col in df:
+    print("Nombre col: " + col)
+    x_axis = np.array([0, len(df)])
+    print(x_axis)
+    y_axis = df[col].values
+    plt.pyplot.plot(x_axis, y_axis)
+    plt.show()
+
